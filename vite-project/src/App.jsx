be2de38043843d0d74a97b0a214ecd19c4125ad8
NotMe <notMe@not.com>
2024-05-20 11:03:12 -0500

    new project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './pages/Home/Home'
+import Navbar from './pages/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
