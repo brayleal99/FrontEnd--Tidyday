46ca238d561eb4985a1a78f5b483d5d90d272c5b
NotMe <notMe@not.com>
2024-05-21 08:56:58 -0500

    update

--- vite-project/src/pages/project/CreateProjectForm.jsx
+++ vite-project/src/pages/project/CreateProjectForm.jsx
@@ -1,8 +1,44 @@
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
 
 const CreateProjectForm = () => {
+  const form=useForm({
+    //resolver:zod
+    defaultValues: {
+      name: "",
+      description: "",
+      category: "",
+      tasgs:["javascript", "react"]
+    }
+  })
+
+  const onSubmit=(data) => {
+    console.log("create project data",data);
+  }
   return (
-    <div>CreateProjectForm</div>
+    <div>
+
+      <Form {...form}>
+
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField control={form.control}
+            name="name"
+            render={({ field }) => <FormItem>
+              <FormControl>
+                <Input {...field}
+                  type="" text
+                  className="border w-full border-gray-700 py-5 px-5"
+                  placeholder="project name..." />
+              </FormControl>
+              <FormMessage/>
+            </FormItem>} />
+        </form>
+
+      </Form>
+
+    </div>
   )
 }
 
-export default CreateProjectForm
+export default CreateProjectForm;
